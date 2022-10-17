5ecdbb45472ae8e2e222ce4d25b5c2c62c362dbc
student <adame68@nycstudents.net>
2022-10-17 12:33:26 -0400

    Class

---
+++ src/LinearEquation.java
@@ -0,0 +1,12 @@
+public class LinearEquation {
+    /* Instance Variables */
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+
+    /* Creates a LinearEquation object */
+    public LinearEquation (int x1, int y1, int x2, int y2){
+    }
+}
---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main (String [] args) {
+
+        Scanner scan = new Scanner (System.in);
+        //welcomes the user
+        System.out.println("Welcome!");
+
+        System.out.print("Enter coordinate 1: ");
+        String userInput = scan.nextLine();
+        userInput +=
+    }
+}
