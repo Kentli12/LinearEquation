27dc90a2363b28e71a6cbdd244d421de18b16007
adamelsayed06 <adame68@nycstudents.net>
2022-10-18 12:30:29 -0400

    Classwork

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,3 +1,4 @@
+
 public class LinearEquation {
     /* Instance Variables */
     private int x1;
@@ -8,5 +9,33 @@ public class LinearEquation {
 
     /* Creates a LinearEquation object */
     public LinearEquation (int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
+
+    public double distance(){
+        double xDistance = (x2 - x1);
+        double yDistance = (y2 - y1);
+        double distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+        return distance;
+    }
+    public double slope(){
+        double rise = (y2 - y1);
+        double run = (x2 - x1);
+        double slope = rise/run;
+        return slope;
+    }
+
+    public double yIntercept(){
+        double x =  x2  * slope();
+        double yIntercept = y2- x;
+        return yIntercept;
+    }
+
+    public String equation(){
+        String equation = "y "
+    }
+
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+
 public class LinearEquationRunner {
     public static void main (String [] args) {
 
@@ -6,8 +8,30 @@ public class LinearEquationRunner {
         //welcomes the user
         System.out.println("Welcome!");
 
+        //gets the first coordinates and stores it as integer variables
         System.out.print("Enter coordinate 1: ");
         String userInput = scan.nextLine();
-        userInput +=
+        int comma = userInput.indexOf(",");
+        String stringX = userInput.substring(1, comma);
+        String stringY = userInput.substring(comma +2, userInput.length()-1);
+        int x1 = Integer.parseInt(stringX);
+        int y1 = Integer.parseInt(stringY);
+        System.out.println(" " + x1 + " " +y1);
+
+        //gets the second coordinates and stores it as integer variables
+        System.out.print("Enter coordinate 2: ");
+        String userInput2 = scan.nextLine();
+        comma = userInput2.indexOf(",");
+        String stringX2 = userInput2.substring(1, comma);
+        String stringY2 = userInput2.substring(comma +2, userInput2.length()-1);
+        int x2 = Integer.parseInt(stringX2);
+        int y2 = Integer.parseInt(stringY2);
+        System.out.println(" " + x2 + " " +y2);
+
+        LinearEquation equa1 = new LinearEquation(x1,y1,x2,y2);
+        System.out.println(equa1.distance());
+        System.out.println(equa1.slope());
+        System.out.println(equa1.yIntercept());
+
     }
 }
