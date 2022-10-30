d0b1f5c64aef4d28cbbda61ca26fdf9af1d83bc0
Kent Li <kentl5@nycstudents.net>
2022-10-30 18:39:20 -0400

    Done

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -14,7 +14,7 @@ public class LinearEquation {
         this.x2 = x2;
         this.y2 = y2;
     }
-
+    //method that returns the distance, rounded to the nearest hundredth
     public double distance() {
         double xDistance = (double) (x2 - x1);
         double yDistance = (double) (y2 - y1);
@@ -22,29 +22,32 @@ public class LinearEquation {
         double distance = Math.sqrt(squared);
         return roundedToHundredth(distance);
     }
-
+    //method that returns the slope, rounded to the nearest hundredth
     public double slope() {
         double riseS = (double) (y2 - y1);
         double runS = (double) (x2 - x1);
         double slope = riseS / runS;
         return roundedToHundredth(slope);
     }
-
+    //method that returns the yIntercept, rounded to the nearest hundredth
     public double yIntercept() {
         double x = x2 * slope();
         double yIntercept = y2 - x;
         return roundedToHundredth(yIntercept);
     }
 
-
+    //method that returns the equation of the two points, rounded to the nearest hundredth
     public String equation() {
+    //declares and initializes variables used in the code
         int rise = y2 - y1;
         int run = x2 - x1;
         double yIntcep = yIntercept();
+    //determines if slope is 0 and returns the correct equation
         if (rise / run == 0) {
             String equation = ("y = " + (int)yIntercept());
             return equation;
         } else {
+    //determines whether the slope is a whole number
             if (slope() % 1 == 0) {
                 if (yIntcep == 0) {
                     String equation = ("y = " + (int) slope() + "x  ");
@@ -56,9 +59,10 @@ public class LinearEquation {
                     String equation = ("y = " + (int) slope() + "x" + " + " + yIntercept());
                     return equation;
                 }
+    //this part of the code will run if the slope is a fraction
             } else {
+    //this part of the code will run if both the numerator and denominator are negative
                 if (rise < 0 && run < 0) {
-
                     if (yIntcep == 0) {
                         String equation = ("y = " + rise + "/" + run + "x  ");
                         return equation;
@@ -69,6 +73,7 @@ public class LinearEquation {
                         String equation = ("y = " + rise + "/" + run + "x" + " + " + yIntercept());
                         return equation;
                     }
+    //this part of the code will run, if either the numerator or denominator contains a negative
                 } else if (run < 0) {
                     rise = Math.abs(rise);
                     run = Math.abs(run);
@@ -82,6 +87,7 @@ public class LinearEquation {
                         String equation = ("y = " + "-" + rise + "/" + run + "x" + " + " + yIntercept());
                         return equation;
                     }
+    //this part of the code will run, if both the numerator and denominator are positive
                 } else {
                     if (yIntcep == 0) {
                         String equation = ("y = " + rise + "/" + run + "x  ");
@@ -99,10 +105,11 @@ public class LinearEquation {
         }
     }
 
-
+    //helper method that rounds a number to the nearest hundredth
         public double roundedToHundredth(double toRound){
             return Math.round(toRound*100.0)/100.0;
         }
+    //returns a string with all the information of the linear equation
         public String lineInfo(){
             return   ("\n"+ "The two points are: (" + x1 + ", " + y1+ ") and (" + x2 + ", " + y2 + ")"
                     + "\n" + "The equation between these two points is: " + equation()
@@ -110,7 +117,7 @@ public class LinearEquation {
                     + "\n" + "The y-intercept of the line is: " + yIntercept()
                     + "\n" + "The distance between the two points is " + distance());
         }
-
+    //returns a specific coordinate based on the x-value
         public String coordinateForX(double xvalue){
             double y = roundedToHundredth(slope() * xvalue + yIntercept());
             return ("These points are a vertical line is " + "(" + xvalue +", " + y + ")");
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class LinearEquationRunner {
     public static void main (String [] args) {
-
+        //creates the scanner
         Scanner scan = new Scanner (System.in);
         //welcomes the user
         System.out.println("Welcome!");
