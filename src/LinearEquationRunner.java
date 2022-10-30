20c80a5e56562357032590a98a4031a39bf7c186
Kent Li <kentl5@nycstudents.net>
2022-10-30 19:20:37 -0400

    Done

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -44,8 +44,6 @@ public class LinearEquationRunner {
             double valueX = scan.nextDouble();
             System.out.println(linearEquation.coordinateForX(valueX));
         }
-
-
-
+        scan.close();
     }
 }
